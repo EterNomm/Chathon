bf2d4b56dd378d6c73a267386d745532a2bed670
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-23 10:23:33 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import re
+
+with open('README.md') as f:
+    long_description = f.read()
+
+version = ''
+with open('chathon/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Version is not set')
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
+
+#####################################
+#VERSION = "1.0"
+#####################################
+
+setup(
+    name='chathon',
+    version=version,
+    author='LyQuid',
+    author_email='lyquidpersonal@gmail.com',
+    description = 'A Python Packages to make own chat room',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    url='https://github.com/EterNomm/Chathon',
+    project_urls={
+        "Discord": "https://discord.gg/qpT2AeYZRN",
+        "Issue tracker": "https://github.com/EterNomm/Chathon/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    keywords=["python", "chathon", "chat", "chat room", "chat app"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Android',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python'
+    ]
+)
