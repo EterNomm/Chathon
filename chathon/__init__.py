7ab8e225754cfdeb1ae1dd8379e1df8bcd6d61b7
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-24 09:37:32 +0700

    [__init__] Updated version to 1.5

--- chathon/__init__.py
+++ chathon/__init__.py
@@ -7,7 +7,7 @@ print("Join EterNomm Discord Here : https://dsc.gg/eternomm")
 print()
 
 __title__ = "Chathon"
-__version__ = "1.2"
+__version__ = "1.5"
 __authors__ = ["LyQuid", "EterNomm"]
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021 EterNomm"
