7f1c4edd9514608a21b065c5882ecd6e5ce3e121
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-24 08:53:02 +0700

    [server] Added Features to record client conversation and added Docstrings

--- chathon/server.py
+++ chathon/server.py
@@ -1,8 +1,16 @@
 import socket
 import threading
+from time import sleep
 
 class Server:
-    def __init__(self, port):
+    r"""A class that implements the server side
+    -----------
+    - ip: :class:`localhost/127.0.0.1` | you cannot change server ip because server ip always use localhost
+    - port: :class:`int` | The server port.
+    - record_conversation: :class:`False/True` | Record all client conversation in your server (if `True`, client will get warning message that your server is record all client conversation)
+    """
+
+    def __init__(self, port:int, record_conversation=False):
         host = '127.0.0.1'
         
         # Starting Server
@@ -10,6 +18,13 @@ class Server:
         server.bind((host, port))
         server.listen()
         print("Server Online !")
+        print()
+        print("Server Information :")
+        print(f"IP : {host} (use your network IP address for client connect to your server)")
+        print(f"PORT : {port}")
+        print(f"Print Client Message : {record_conversation}")
+        print()
+        self.print_log = record_conversation
         self.server = server
 
     def start(self):
@@ -27,13 +42,20 @@ class Server:
                     # Broadcasting Messages
                     message = client.recv(1024)
                     broadcast(message)
+                    if self.print_log == True:
+                        message = str(message)
+                        message = message.replace("'", "")
+                        message = message.replace("b", "")
+                        print(message)
+                    else:
+                        pass
                 except:
                     # Removing And Closing Clients
                     index = clients.index(client)
                     clients.remove(client)
                     client.close()
                     nickname = nicknames[index]
-                    broadcast('{} left!'.format(nickname).encode('ascii'))
+                    broadcast('{} left!'.format(nickname).encode('UTF-8'))
                     print('{} left'.format(nickname))
                     nicknames.remove(nickname)
                     break
@@ -45,15 +67,19 @@ class Server:
                 print("Connected with {}".format(str(address)))
         
                 # Request And Store Nickname
-                client.send('NICK'.encode('ascii'))
-                nickname = client.recv(1024).decode('ascii')
+                client.send('NICK'.encode('UTF-8'))
+                nickname = client.recv(1024).decode('UTF-8')
                 nicknames.append(nickname)
                 clients.append(client)
         
                 # Print And Broadcast Nickname
                 print("New Client : {}".format(nickname))
-                broadcast("{} joined!".format(nickname).encode('ascii'))
-                client.send('Connected to server!'.encode('ascii'))
+                broadcast("{} joined!".format(nickname).encode('UTF-8'))
+                client.send('Connected to server!'.encode('UTF-8'))
+                if self.print_log == True:
+                    client.send("\nWarning, the server may record your conversation.\n\n".encode('UTF-8'))
+                else:
+                    pass
         
                 # Start Handling Thread For Client
                 thread = threading.Thread(target=handle, args=(client,))
