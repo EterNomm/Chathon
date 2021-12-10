237b3b5176467f2bc20d6cbd0a2777c468ea6977
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-10 13:14:28 +0700

    [__init__] Removing EterNomm messages

--- chathon/__init__.py
+++ chathon/__init__.py
@@ -2,10 +2,6 @@ from .client import *
 from .server import *
 from .bot import *
 
-print("Hello from EterNomm!")
-print("Join EterNomm Discord Here : https://dsc.gg/eternomm")
-print()
-
 __title__ = "Chathon"
 __version__ = "1.5"
 __authors__ = ["LyQuid", "EterNomm"]
