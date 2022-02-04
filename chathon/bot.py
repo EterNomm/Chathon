0a3aa3ac308263d8eb30f27cf5d01b3ec78592d0
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-02-04 13:28:14 +0700

    [bot] Now bot function using Decorator

--- chathon/bot.py
+++ chathon/bot.py
@@ -2,8 +2,12 @@ import socket
 import threading
 from time import sleep
 
+def listToStr(s): 
+    string = " " 
+    return (string.join(s))
+
 class Bot:
-    r"""A class that implements the bot engine
+    r"""A class that implements the bot engine.
     -----------
     Parameters :
     - bot_name: :class:`str` | set bot username
@@ -20,48 +24,76 @@ class Bot:
         self.prefix = prefix
         self.bot = bot
         self.user_input = None
+        self.cache = {}
+        self.response = None
 
-    def command(self, command_name, response):
-        r"""A class that implements the bot engine
-        -----------
-        Parameters :
-        - command_name: :class:`str` | command name so that clients can use it, for example: `test` for`/test`
-        - response: :class:`str` | Command response
+    def command(self):
+        """
+        This method for bot command. This method using Decorator.
+        Example : `@bot.command()`.
+        See full example on github.
+        """
+        def decorator(func):
+            self.cache[func.__name__] = func
+
+        return decorator
+
+    def send(self, message:str):
+        """
+        A method to send message when user use command.
+        --
+        Parameter :
+        - message: `str`
+        """
+        self.response = message
+
+    def run(self):
+        """
+        A method to run the bot.
         """
 
         bot = self.bot
         nickname = self.bot_name
-        prefix = self.prefix
+
         def receive():
             while True:
                 try:
-                    # Receive Message From Server
-                    # If 'NICK' Send Nickname
                     message = bot.recv(1024).decode('UTF-8')
                     if message == 'NICK':
                         bot.send(nickname.encode('UTF-8'))
                     else:
                         print(message)
-                        user_msg = message
-                        get_cmd = str(user_msg.split(" ")[1:])
-                        get_cmd = get_cmd.replace("[", "")
-                        get_cmd = get_cmd.replace("]", "")
-                        get_cmd = get_cmd.replace("'", "")
-                        self.user_input = get_cmd
+                        self.user_input = message
                 except:
-                    # Close Connection When Error
                     print("An error occured!")
                     bot.close()
                     break
 
         def write():
             while True:
-                if self.user_input == f"{prefix}{command_name}":
-                    message = '{}: {}'.format(f"{nickname} [BOT]", response)
-                    bot.send(message.encode('UTF-8'))
-                    sleep(1) # prevent spam
-                else:
+                prefix = self.prefix
+                ui = self.user_input
+                ui = str(ui)
+                ui_replace = ui.split(": ")[1:]
+                ui_replace = listToStr(ui_replace)
+                arguments = ui_replace.split(" ")[1:]
+                cmd_name = ui_replace.replace(prefix, "")
+                cmd_name = cmd_name.split(" ")[0]
+                get_input = ui_replace.split(" ")[0]
+
+                command_name = self.cache.get(cmd_name)
+
+                if command_name == None:
                     pass
+                
+                else:
+                    if get_input == prefix+cmd_name:
+                        command_name(*arguments)
+                        message = '{}: {}'.format(f"{nickname} [BOT]", self.response)
+                        bot.send(message.encode('UTF-8'))
+                        sleep(1) # prevent spam
+                    else:
+                        pass
 
         receive_thread = threading.Thread(target=receive)
         receive_thread.start()
