0c4d62c39776da95a8e8b864c9dd81090ced1f84
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-02-06 13:37:48 +0700

    [Server] Added `set_welcome_message` function

--- chathon/server.py
+++ chathon/server.py
@@ -1,35 +1,53 @@
 import socket
 import threading
 from time import sleep
+import colorgb
 
 class Server:
-    r"""A class that implements the server side
+    """A class that implements the server side.
     -----------
     Parameters :
-    - ip: :class:`localhost/127.0.0.1` | you cannot change server ip because server ip always use localhost
+    - ip: :class:`localhost/127.0.0.1` | you cannot change server ip because server ip always use localhost.
     - port: :class:`int` | The server port.
-    - record_conversation: :class:`False/True` | Record all client conversation in your server (if `True`, client will get warning message that your server is record all client conversation)
+    - record_conversation: :class:`False/True` | Record all client conversation in your server (if `True`, client will get warning message that your server is record all client conversation).
     """
 
     def __init__(self, port:int, record_conversation=False):
         host = '127.0.0.1'
         
-        # Starting Server
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((host, port))
         server.listen()
-        print("Server Online !")
         print()
         print("Server Information :")
+        print(f"Status : {colorgb.fore('Online', 'lgreen')}")
         print(f"IP : {host} (use your network IP address for client connect to your server)")
         print(f"PORT : {port}")
-        print(f"Print Client Message : {record_conversation}")
+        if record_conversation == True:
+            record_conversation = colorgb.fore(True, 'lred')
+        else:
+            record_conversation = colorgb.fore(True, 'lgreen')
+        print(f"Record Conversation : {record_conversation}")
         print()
         self.print_log = record_conversation
         self.server = server
+        self.welcome_msg = None
+
+    def set_welcome_message(self, message:str):
+        """
+        Set welcome message when user join (Can only be seen by the user).
+        -----
+        Parameter :
+        - message: `str`
+        """
+        message = f"{message}\n"
+        self.welcome_msg = message
+        print(f"Welcome message :")
+        print(message)
+        print()
 
     def start(self):
-        r"""Nothing, just function to start the server
+        """Nothing, just function to start the server.
         -----------
         """
         clients = []
@@ -43,18 +61,13 @@ class Server:
         def handle(client):
             while True:
                 try:
-                    # Broadcasting Messages
                     message = client.recv(1024)
                     broadcast(message)
                     if self.print_log == True:
-                        message = str(message)
-                        message = message.replace("'", "")
-                        message = message.replace("b", "")
-                        print(message)
+                        print(message.decode('UTF-8'))
                     else:
                         pass
                 except:
-                    # Removing And Closing Clients
                     index = clients.index(client)
                     clients.remove(client)
                     client.close()
@@ -66,26 +79,28 @@ class Server:
         
         def receive():
             while True:
-                # Accept Connection
                 client, address = server.accept()
                 print("Connected with {}".format(str(address)))
         
-                # Request And Store Nickname
                 client.send('NICK'.encode('UTF-8'))
                 nickname = client.recv(1024).decode('UTF-8')
                 nicknames.append(nickname)
                 clients.append(client)
         
-                # Print And Broadcast Nickname
                 print("New Client : {}".format(nickname))
                 broadcast("{} joined!".format(nickname).encode('UTF-8'))
+                if self.welcome_msg == None:
+                    pass
+                else:
+                    client.send(self.welcome_msg.encode('UTF-8'))
+
                 client.send('Connected to server!'.encode('UTF-8'))
                 if self.print_log == True:
-                    client.send("\nWarning, the server may record your conversation.\n\n".encode('UTF-8'))
+                    msg = colorgb.fore("Warning, the server may record your conversation.", "lred")
+                    client.send(f"\n{msg}\n\n".encode('UTF-8'))
                 else:
                     pass
         
-                # Start Handling Thread For Client
                 thread = threading.Thread(target=handle, args=(client,))
                 thread.start()
             
