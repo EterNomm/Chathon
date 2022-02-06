3feb0ef0ce500b5ae83471d3124c387539538925
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-02-06 10:30:58 +0700

    [`__init__`] update version to `2.0b1`

--- chathon/__init__.py
+++ chathon/__init__.py
@@ -3,7 +3,7 @@ from .server import *
 from .bot import *
 
 __title__ = "Chathon"
-__version__ = "2.0b"
+__version__ = "2.0b1"
 __authors__ = ["LyQuid", "EterNomm"]
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021 EterNomm"
